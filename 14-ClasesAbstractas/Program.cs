71004aced5191951e0b933574b7d1fe668259d01
Albano Díez de Paulino <albanodiezp@gmail.com>
2023-02-21 23:20:50 +0100

    Aprendizaje 21/02/2023

---
+++ 14-ClasesAbstractas/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Interface;
+
+namespace ClasesAbstractas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Caballo babieca = new Caballo("Babieca");
+            IMamiferosTerrestres IBabieca = babieca;
+            Humano Albano = new Humano("Albano");
+            Gorila copito = new Gorila("Copito");
+
+            Mamifero Bucefalo = new Caballo("Bucefalo");
+            //babieca.getnombreSerVivo();
+
+            Ballena miWally = new Ballena("Wally");
+            miWally.nadar();
+
+            Console.WriteLine(IBabieca.numeroPatas());*/
+
+            Lagartija Juancho=new Lagartija("Juancho");
+            Juancho.respirar();
+            Humano Albano = new Humano("Albano");
+            Albano.respirar();
+
+
+        }
+    }
+    abstract class Animales
+    {
+        public void respirar()
+        {
+            Console.WriteLine("Soy capaz de respirar");
+        }
+
+        public abstract void getNombre();
+
+    }
+    class Mamifero: Animales
+    {
+        public Mamifero(String nombre)
+        {
+            nombreSerVivo = nombre;
+        }
+        public virtual void pensar()
+        {
+            Console.WriteLine("pensamiento basico");
+        }
+        public void cuidarCrias()
+        {
+            Console.WriteLine("Cuido de mis crias hasta que se valgan por si solas");
+        }
+        public override void getNombre()
+        {
+            Console.WriteLine(nombreSerVivo);
+        }
+
+        private String nombreSerVivo;
+
+    }
+    class Caballo : Mamifero, IMamiferosTerrestres, IAnimalesEnDeportes, ISaltoConPatas
+    {
+        public Caballo(String nombreCaballo) : base(nombreCaballo)
+        {
+
+        }
+        public void galopar()
+        {
+            Console.WriteLine("Soy capaz de galopar");
+        }
+        int IMamiferosTerrestres.numeroPatas()
+        {
+            return 4;
+        }
+        public string tipoDeporte()
+        {
+            return "En ipica";
+        }
+        public Boolean esOlimpico()
+        {
+            return true;
+        }
+        int ISaltoConPatas.numeroPatas()
+        {
+            return 2;
+        }
+
+    }
+    class Humano : Mamifero
+    {
+        public Humano(String nombreHumano) : base(nombreHumano)
+        {
+
+        }
+        public override void pensar()
+        {
+            Console.WriteLine("Soy capaz de pensar");
+        }
+    }
+    class Gorila : Mamifero, IMamiferosTerrestres
+    {
+        public Gorila(String nombreGorila) : base(nombreGorila)
+        {
+
+        }
+        public void trepar()
+        {
+            Console.WriteLine("Soy capaz de trepar");
+        }
+        public int numeroPatas() { return 2; }
+    }
+    class Ballena : Mamifero
+    {
+        public Ballena(String nombreBallena) : base(nombreBallena)
+        {
+
+        }
+        public void nadar() { Console.WriteLine("Soy capaz de nadar"); }
+    }
+
+    class Lagartija : Animales
+    {
+
+        private String nombreReptil;
+
+        public Lagartija(string nombreReptil)
+        {
+            this.nombreReptil = nombreReptil;
+        }
+
+        public override void getNombre()
+        {
+            Console.WriteLine(nombreReptil);
+        }
+    }
+}
