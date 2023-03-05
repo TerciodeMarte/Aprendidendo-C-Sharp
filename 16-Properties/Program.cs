1e483f5ead63964aa958950f5e962bbb87a60144
Albano Díez de Paulino <albanodiezp@gmail.com>
2023-03-05 17:28:04 +0100

    Properties

---
+++ 16-Properties/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado albano = new Empleado("Albano");
+
+            albano.SALARIO = 1200;
+
+            Console.WriteLine("El salario de Albano es: " + albano.SALARIO);
+
+        }
+
+        class Empleado
+        {
+            private double salario;
+            private string nombre;
+
+            public Empleado(string nombre)
+            {
+                this.nombre = nombre;
+            }
+
+            /*public double getSalario()
+            {
+                return salario;
+            }
+            public void setSalario(double salario)
+            {
+                if (salario < 0)
+                {
+                    Console.WriteLine("El salario no puede ser Negativo,se le asigna 0");
+                    this.salario = 0;
+                }
+                else
+                {
+                    this.salario = salario;
+                }
+
+            }*/
+            public double evaluaSalario(double salario)
+            {
+                if (salario < 0)
+                {
+                    Console.WriteLine("El salario no puede ser Negativo,se le asigna 0");
+                    return 0;
+                }
+                else
+                {
+                    return salario;
+                }
+
+            }
+            //CREACION DE PROPIEDAD
+
+           /* public double SALARIO
+            {
+                get { return this.salario; }
+
+                set { this.salario = evaluaSalario(value); }
+
+            }*/
+
+            public double SALARIO
+            {
+                get=>this.salario;
+                set => this.salario = evaluaSalario (value);
+            }
+        }
+    }
+}
