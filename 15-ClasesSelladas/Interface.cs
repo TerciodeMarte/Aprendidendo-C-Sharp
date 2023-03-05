9a2810919648d2a6e20ff838b4b1c1754d5325e5
Albano Díez de Paulino <albanodiezp@gmail.com>
2023-03-05 16:51:16 +0100

    Clases Selladas

---
+++ 15-ClasesSelladas/Interface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Interface
+{
+    interface IMamiferosTerrestres
+    {
+        int numeroPatas();
+    }
+    interface IAnimalesEnDeportes
+    {
+        string tipoDeporte();
+        Boolean esOlimpico();
+    }
+    interface ISaltoConPatas
+    {
+        int numeroPatas();
+    }
+}
